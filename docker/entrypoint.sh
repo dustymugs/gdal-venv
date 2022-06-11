225b497c51325757492d908d1869d14407601ef9
Bborie Park <dustymugs@gmail.com>
2022-06-11 13:14:51 -0700

    automate import of GDAL

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,18 +3,26 @@
 if [ "$1" == version ]; then
   . $USER_VENV/bin/activate
 
+  set -e
+
   cd /home/pygdal
   git clone https://${GITHUB_TOKEN}:@github.com/dustymugs/pygdal.git pygdal
+
   cd pygdal
+  git remote set-url origin "https://${GITHUB_TOKEN}:@github.com/dustymugs/pygdal.git"
 
   VERSION="$2"
-  git checkout -b "v$VERSION"
-  python import.py "$2"
-  git add .
+  BRANCH="v$VERSION"
+  MESSAGE="add support for $VERSION"
 
-  message = "add support for v$VERSION"
-  git commit -m "$message"
-  gh pr create -B master -t "$message"
+  git checkout -b "$BRANCH"
+  python import.py "$VERSION"
+  echo "$VERSION" >> VERSIONS
+  git add .
+  git commit -m "$MESSAGE"
+  git push origin "$BRANCH"
+  gh pr create -B master --title "$MESSAGE" --body "Done by deploy docker image"
+	gh pr merge -m -b "automated merge" "$BRANCH}"
 else
   exec "$@"
 fi
