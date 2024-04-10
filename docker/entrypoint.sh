ff493cccd32a12b5f6332338287f0c986c42d137
Bborie Park <dustymugs@gmail.com>
2024-04-10 14:28:31 -0700

    cleanup of pygdal to gdal-venv

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,14 +5,14 @@ if [ "$1" == version ]; then
 
   set -e
 
-  cd /home/pygdal
-  git clone https://${GITHUB_TOKEN}:@github.com/dustymugs/pygdal.git pygdal
+  cd /home/gdal
+  git clone https://${GITHUB_TOKEN}:@github.com/dustymugs/gdal-venv.git gdal-venv
 
-  cd pygdal
-  git remote set-url origin "https://${GITHUB_TOKEN}:@github.com/dustymugs/pygdal.git"
+  cd gdal-venv
+  git remote set-url origin "https://${GITHUB_TOKEN}:@github.com/dustymugs/gdal-venv.git"
 
   VERSION="$2"
-	PUBLISH="$3"
+  PUBLISH="$3"
   BRANCH="v$VERSION"
   MESSAGE="Add GDAL $VERSION"
 
@@ -30,29 +30,29 @@ if [ "$1" == version ]; then
   gh pr create -B master --title "$MESSAGE" --body "Done by deploy docker image"
   gh pr merge -m -b "automated merge" "$BRANCH"
 
-	if [ -n "$PUBLISH" ]; then
-		cd /tmp
-		mkdir gdal
-		tar xf $LOCAL -C gdal --strip-components=1
-		cd gdal
-		if [ -d ./gdal ]; then
-			cd gdal
-		fi
-		if [ -f ./CMakeLists.txt ]; then
-			mkdir build
-			cd build
-			cmake ..
-			cmake --build .
-		else
-			./autogen.sh
-			./configure
-			make
-		fi
-		export PATH="$(pwd)/apps:$PATH"
+  if [ -n "$PUBLISH" ]; then
+    cd /tmp
+    mkdir gdal
+    tar xf $LOCAL -C gdal --strip-components=1
+    cd gdal
+    if [ -d ./gdal ]; then
+      cd gdal
+    fi
+    if [ -f ./CMakeLists.txt ]; then
+      mkdir build
+      cd build
+      cmake ..
+      cmake --build .
+    else
+      ./autogen.sh
+      ./configure
+      make
+    fi
+    export PATH="$(pwd)/apps:$PATH"
 
-		cd /home/pygdal/pygdal
-		./publish $VERSION
-	fi
+    cd /home/gdal/gdal-venv
+    ./publish $VERSION
+  fi
 else
   exec "$@"
 fi
